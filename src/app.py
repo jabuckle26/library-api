0faa2590e4cd06d2e5836601e9d2c1edc86f4df8
Jabuckle26 <jabuckle@tcd.ie>
2021-09-05 15:27:08 +0100

    Added member routes.

--- src/app.py
+++ src/app.py
@@ -2,12 +2,13 @@ import os
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from routes import book, book_genre
+from routes import book, book_genre, member
 
 
 app = FastAPI()
 app.include_router(book.router)
 app.include_router(book_genre.router)
+app.include_router(member.router)
 
 
 if __name__ == "__main__":
