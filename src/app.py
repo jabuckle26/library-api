66788eeddb1c260350e6d8d52d8b11d1179ca29f
Jabuckle26 <jabuckle@tcd.ie>
2021-11-07 20:42:26 +0000

    Add localhost cors config.

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import os
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import book, book_genre, borrowed_books, member
 
 
@@ -11,6 +12,17 @@ app.include_router(book_genre.router)
 app.include_router(borrowed_books.router)
 app.include_router(member.router)
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", port=int(os.getenv('PORT')),
